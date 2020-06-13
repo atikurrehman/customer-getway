b3c2710eebbfebf098e2bbf9a8fb56a749ef6bac
atikurrehman <atikurrehmank@gmail.com>
2020-06-13 17:46:39 +0530

    get-way initilasization

---
+++ src/main/java/com/example/customergetway/CustomerGetwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.customergetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerGetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerGetwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customergetway/CustomerGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customergetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerGetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
